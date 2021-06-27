c8366a14acf9d3f2b73e1c3fe8820c03d7c50c6c
zhangting <git@github.com:1458696128/react-admin.git>
2021-06-27 22:38:46 +0800

    zt

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React  from 'react'
+import 'antd/dist/antd.less'
+import {HashRouter,Route,Switch} from 'react-router-dom'
+import  Login from './pages/login/login'
+import  Admin  from './pages/admin/admin'
+export default class App extends React.Component{
+    render(){
+        return(
+           <HashRouter>
+               <Switch>
+                   <Route path='/login' component={Login}></Route>
+                   <Route path='/' component={Admin}></Route>
+               </Switch>
+           </HashRouter>
+        ) 
+    }
+} 
